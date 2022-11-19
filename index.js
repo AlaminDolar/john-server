7784e71145c406897276bc994c387a33bce8a683
AlaminDolar <amin15-9898@diu.edu.bd>
2022-11-19 20:41:40 +0600

    pagination done

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+const app = express();
 
 // middleware
 app.use(cors());
 app.use(express());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uawimcd.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("emaJohn").collection("product");
+
+    app.get("/product", async (req, res) => {
+      console.log("query", req.query);
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const query = {};
+
+      const cursor = productCollection.find(query);
+      if (page || size) {
+        prodcts = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        prodcts = await cursor.toArray();
+      }
+
+      res.send(prodcts);
+    });
+
+    app.get("/productCount", async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("john is running at server by app.get method");
